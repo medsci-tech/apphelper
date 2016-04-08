1cef885c5577900e0826da4a4e94a8dacefd640b
lostofland <772932587@qq.com>
2016-04-09 01:14:45 +0800

    php7 session_regenerate_id bug问题

--- backend/config/main-local.php
+++ backend/config/main-local.php
@@ -1,9 +1,13 @@
 <?php
 
 $config = [];
-
+if (YII_ENV_DEV) {
+    // 根据 `dev` 环境进行的配置调整
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = 'yii\debug\Module';
+}
 if (!YII_ENV_TEST) {
-    // configuration adjustments for 'dev' environment
+    // 以下配置项仅在测试环境中起作用
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = 'yii\debug\Module';
     $config['modules']['gii'] = 'yii\gii\Module';
