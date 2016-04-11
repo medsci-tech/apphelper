77f77bd13fb5b266b618526273acc13bcd26e442
fantasylxh <772932587@qq.com>
2016-04-11 16:46:30 +0800

    v1 demo

--- api/common/controllers/SiteController.php
+++ api/common/controllers/SiteController.php
@@ -4,11 +4,24 @@ namespace api\common\controllers;
 
 use common\logic\Article;
 use yii\rest\Controller;
+use yii\web\Response;
 
 class SiteController extends Controller
 {
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['contentNegotiator']['formats'] = '';
+        $behaviors['contentNegotiator']['formats']['application/json'] = Response::FORMAT_JSON;
+        return $behaviors;
+    }
     public function actionIndex()
     {
         return Article::find()->limit(1)->one();
+    }
+
+    public function actionTest()
+    {
+        return ['code'=>200,'','data'=>['a','b','c']];
     }
 }
