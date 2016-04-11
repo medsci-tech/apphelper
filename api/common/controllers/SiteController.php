448b24a88a4f7573aa9eaca6ffdb7605fdf4de49
fantasylxh <772932587@qq.com>
2016-04-11 17:37:27 +0800

    集成common

--- api/common/controllers/SiteController.php
+++ api/common/controllers/SiteController.php
@@ -3,18 +3,10 @@
 namespace api\common\controllers;
 
 use common\logic\Article;
-use yii\rest\Controller;
-use yii\web\Response;
 
 class SiteController extends Controller
 {
-    public function behaviors()
-    {
-        $behaviors = parent::behaviors();
-        $behaviors['contentNegotiator']['formats'] = '';
-        $behaviors['contentNegotiator']['formats']['application/json'] = Response::FORMAT_JSON;
-        return $behaviors;
-    }
+
     public function actionIndex()
     {
         return Article::find()->limit(1)->one();
