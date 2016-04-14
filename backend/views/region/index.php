32b79e15f528f7f9791d32b7f69e61b9253a7039
zhaiyu <zhaiyu_hb@hotmail.com>
2016-04-14 10:52:46 +0800

    添加layerjs文件和修改省市区联动

--- backend/views/region/index.php
+++ backend/views/region/index.php
@@ -7,28 +7,28 @@ $htmlClass = 'form-group container-fluid';
 ?>
 <div class="form-inline form-group">
 <?php //$form = ActiveForm::begin(['enableClientValidation' => false]);?>
-<?= $form->field($model,'province_id', ['options' => ['class' => $htmlClass]])->dropDownList($model->getRegionList(0),
+<?= $form->field($model,'province_id', ['options' => ['class' => $htmlClass]])->dropDownList($model->getRegionList(0,1),
     [
         'prompt'=>'--请选择省--',
         'name'=>$m."[province_id]",
         'onchange'=>'
             $(".form-group.field-region-area_id").hide();
-            $.post("'.yii::$app->urlManager->createUrl('region/list').'?typeid=1&pid="+$(this).val(),function(data){
+            $.post("'.yii::$app->urlManager->createUrl('region/list').'?grade=2&pid="+$(this).val(),function(data){
                 $("select#region-city_id").html(data);
             });',
     ])->label('省份', ['class' => 'sr-only']); ?>
 
-<?= $form->field($model, 'city_id', ['options' => ['class' => $htmlClass]])->dropDownList($model->getRegionList($model->province_id),
+<?= $form->field($model, 'city_id', ['options' => ['class' => $htmlClass]])->dropDownList($model->getRegionList($model->province_id, 2),
     [
         'prompt'=>'--请选择市--',
         'name'=>$m."[city_id]",
         'onchange'=>'
             $(".form-group.field-region-area_id").show();
-            $.post("'.yii::$app->urlManager->createUrl('region/list').'?typeid=2&pid="+$(this).val(),function(data){
+            $.post("'.yii::$app->urlManager->createUrl('region/list').'?grade=3&pid="+$(this).val(),function(data){
                 $("select#region-area_id").html(data);
             });',
     ])->label('市', ['class' => 'sr-only']);  ?>
-<?= $form->field($model, 'area_id', ['options' => ['class' => $htmlClass]])->dropDownList($model->getRegionList($model->city_id),
+<?= $form->field($model, 'area_id', ['options' => ['class' => $htmlClass]])->dropDownList($model->getRegionList($model->city_id,3),
     [
         'prompt'=>'--请选择区--',
         'name'=>$m."[area_id]",
