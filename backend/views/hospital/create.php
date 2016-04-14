ba899b7328f003265bd10c6eb2d36196fd6e2136
fantasylxh <772932587@qq.com>
2016-04-14 16:03:01 +0800

    去掉header title

--- backend/views/hospital/create.php
+++ backend/views/hospital/create.php
@@ -16,9 +16,6 @@ $this->params['breadcrumbs'][] = ['label' => '单位', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="hospital-create">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
     <?= $this->render('_form', [
         'model' => $model,
     ]) ?>
