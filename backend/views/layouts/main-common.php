867078012fa5017db8e08201bc22924774e5d01a
fantasylxh <772932587@qq.com>
2016-04-18 17:33:10 +0800

    add common assert

--- backend/views/layouts/main-common.php
+++ backend/views/layouts/main-common.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 
 /* @var $this \yii\web\View */
 /* @var $content string */
-
+backend\assets\AppAsset::register($this);
 dmstr\web\AdminPlusAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
