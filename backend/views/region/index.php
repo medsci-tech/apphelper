86514eca7679da28c30d2efaec8a5e9687c0c2cc
mime <zhaiyu_hb@hotmail.com>
2016-05-06 17:07:28 +0800

    地区联动优化

--- backend/views/region/index.php
+++ backend/views/region/index.php
@@ -1,26 +1,11 @@
 <?php
-//$this->params['params'] = $label;
-//var_dump($this);
+$city = $regionValue ?? '';
 ?>
 <link href="/css/city-picker.css" rel="stylesheet">
 
-    <div style="position: relative;min-width: 330px;">
-        <input id="city-picker" class="form-control" type="text" value="" data-toggle="city-picker">
-    </div>
+<div style="position: relative;min-width: 330px;">
+    <input id="city-picker" class="form-control" type="text" value="<?php echo $city;?>" data-toggle="city-picker">
+</div>
 
-
-<?php
-$js = <<<JS
-    var city = $('#city-picker');
-       $('#city-picker').citypicker({
-        province: '江苏省',
-        city: '常州市',
-        district: '溧阳市'
-    });
-        
-JS;
-
-$this->registerJS($js);
-?>
 
 
