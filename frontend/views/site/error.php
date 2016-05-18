4495402f3006108463e0ace8534d7eeec1e16457
fantasylxh <772932587@qq.com>
2016-05-18 12:19:14 +0800

    添加wap

--- frontend/views/site/error.php
+++ frontend/views/site/error.php
@@ -1,15 +1,16 @@
 <?php
 
+use yii\helpers\Html;
+
 /* @var $this yii\web\View */
 /* @var $name string */
 /* @var $message string */
 /* @var $exception Exception */
 
-use yii\helpers\Html;
-
 $this->title = $name;
 ?>
-<div class="site-error">
+<section>
+    <article class="site-error">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
@@ -17,11 +18,5 @@ $this->title = $name;
         <?= nl2br(Html::encode($message)) ?>
     </div>
 
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
-
-</div>
+    </article>
+</section>
---
+++ frontend/views/site/view.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no">
+    <title>文章详情</title>
+</head>
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+    }
+    body{
+        background-color: #fff;
+        font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    }
+    .article{
+        margin-left: auto;
+        margin-right: auto;
+        padding-bottom: 100px;
+    }
+    .content{
+        position: relative;
+        padding: 20px 15px 15px;
+        background-color: #fff;
+    }
+    .article_title{
+        margin-bottom: 10px;
+        line-height: 1.4;
+        font-weight: 400;
+        font-size: 24px;
+    }
+    .article_time{
+        font-size: 16px;
+        margin-bottom: 18px;
+        color: #8c8c8c;
+        line-height: 20px;
+    }
+    .article_details{
+        color: #3e3e3e;
+    }
+
+</style>
+<body>
+    <div class="article">
+        <div class="content">
+            <h2 class="article_title">农业兄弟</h2>
+            <div class="article_time">2015-04-12</div>
+            <div class="article_details">中国新农业营销策划第一团队
+
+                三兄弟：
+                同学情：同为黑土地之子，19年前大学校园共居一寝室，上学时候的梦想就是兄弟们能共同组建一个平台，彼此成就！毕业后虽天南海北，但从来没有忘却彼此共同成就的初心！
+
+                农业梦：2007年是一个春天，情聚梦聚北京，斗室之内唱着《大地的孩子》创办蓝狮农业品牌营销策划机构，甘苦自知，八年彼此支撑，将心植入服务二百余农企，携手搭建了从调研到执行，系统的品牌农业全案服务体系。
+
+                立品牌：讲策略，重营销，创意为心，指导执行，一切只为立起品牌新农业、新农人。</div>
+        </div>
+    </div>
+</body>
+</html>
