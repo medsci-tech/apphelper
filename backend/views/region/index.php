687efd5cf57beae7dc715b331cf318f8e49177b2
mime <zhaiyu_hb@hotmail.com>
2016-05-18 13:06:04 +0800

    添加修改操作状态提示

--- backend/views/region/index.php
+++ backend/views/region/index.php
@@ -3,7 +3,7 @@ $regionValue = $regionValue ?? '';
 ?>
 <link href="/css/city-picker.css" rel="stylesheet">
 <div style="position: relative;min-width: 330px;">
-    <input class="form-control" type="text" value="<?php echo $regionValue;?>" data-toggle="city-picker">
+    <input readonly class="form-control" type="text" value="<?php echo $regionValue;?>" data-toggle="city-picker">
 </div>
 <div data-toggle="city-picker-region" style="display: none;"></div>
 
