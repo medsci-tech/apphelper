2ab1670e05a3b7088e48b901257978b754efde95
mime <zhaiyu_hb@hotmail.com>
2016-05-24 12:44:27 +0800

    文件上传

--- backend/views/webuploader/index.php
+++ backend/views/webuploader/index.php
@@ -6,29 +6,38 @@
  * Time: 16:32
  */
 $modelName = $name ?? '';
+$imgMaxSize = $imgMaxSize ?? 2097152;
 $proBarMaxWidth = $proBarWidth ?? 200;
+
 ?>
 
 <div class="form-inline">
     <div class="form-group">
-        <input readonly type="text" class="form-control" data-toggle="progressInput">
-        <input type="hidden" data-toggle="saveInput" name="<?php echo $modelName;?>">
+        <input readonly type="text" class="form-control" data-toggle="upload-progressInput">
+        <input type="hidden" data-toggle="upload-saveInput" name="<?php echo $modelName;?>">
     </div>
-    <button id="promptzone" type="button" class="btn btn-warning">上传</button>
+    <button id="upload-promptzone" type="button" class="btn btn-warning">上传</button>
     <div class="form-group progress">
-        <div id="progressbar" aria-valuemax="100" role="progressbar" class="progress-bar progress-bar-info">
+        <div id="upload-progressbar" aria-valuemax="100" role="progressbar" class="progress-bar progress-bar-info">
             <span></span>
         </div>
     </div>
 </div>
 <?php
+$formatterSize = Yii::$app->formatter->asShortSize($imgMaxSize);
 $js = <<<JS
 // Set fieldname
 	$.ajaxUploadSettings.name = 'file';
-	var probar = $('#progressbar');
+	var probar = $('#upload-progressbar');
 	var progressbarMaxWidth = $proBarMaxWidth;
-	$('#promptzone').ajaxUploadPrompt({
+	$('#upload-promptzone').ajaxUploadPrompt({
 		url : '/upload/img',
+		beforeSend : function(e,f) {
+		    if(f.files[0].size > $imgMaxSize){
+                uploadResultError('文件不能超过' + '$formatterSize');
+                return false;
+		    }
+		},
 		onprogress : function (e) {
 		    probar.removeClass('progress-bar-danger progress-bar-success');
 		    probar.addClass('progress-bar-info');
@@ -45,7 +54,7 @@ $js = <<<JS
 			if (result.code == 200) {
                 uploadResultSuccess(result.data);
 			}else{
-                uploadResultError();
+                uploadResultError(result.msg);
 			}
 		}
 	});
@@ -54,16 +63,20 @@ $js = <<<JS
         probar.removeClass('progress-bar-info');
         probar.addClass('progress-bar-success');
         probar.find('span').text('上传成功');
-        $('[data-toggle="progressInput"]').val(data.tName);
-        $('[data-toggle="saveInput"]').val(data.saveName);
+        $('[data-toggle="upload-progressInput"]').val(data.tName);
+        $('[data-toggle="upload-saveInput"]').val(data.saveName);
 	}
 	
-	function uploadResultError() {
+	uploadResultError = function (msg) {
+	    if(undefined == msg){
+	        msg = '上传失败';
+	    }
         probar.removeClass('progress-bar-info');
         probar.addClass('progress-bar-danger');
-        probar.find('span').text('上传失败');
-        $('[data-toggle="progressInput"]').val('');
-        $('[data-toggle="saveInput"]').val('');
+        probar.find('span').text(msg);
+        probar.css('width', progressbarMaxWidth);
+        $('[data-toggle="upload-progressInput"]').val('');
+        $('[data-toggle="upload-saveInput"]').val('');
 	}
 JS;
 $this->registerJs($js);
