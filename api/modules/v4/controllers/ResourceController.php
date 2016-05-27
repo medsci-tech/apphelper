9e69672c3ae275f3b3d6229d8d019696833bd604
amudogxxx <amudog_1@hotmail.com>
2016-05-27 14:56:34 +0800

    remove log

--- api/modules/v4/controllers/ResourceController.php
+++ api/modules/v4/controllers/ResourceController.php
@@ -76,7 +76,7 @@ class ResourceController extends \api\common\controllers\Controller
                 ->all();
 
             $hour = $time[0]['hours'];
-            print_r($time);
+//            print_r($time);
 
             $z = new ResourceStudyLog();
             $study = $z::find()
@@ -85,7 +85,7 @@ class ResourceController extends \api\common\controllers\Controller
                 ->asArray()
                 ->all();
 
-            print_r($study);
+//            print_r($study);
             $progress = $study[0]['studyTime']/1000/60/$hour;
 
             $row = array('id' => $resource['id'], 'title' => $resource['name'], 'progress' => intval($progress));
