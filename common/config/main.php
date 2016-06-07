3d7be50947057922095f106e25bb650f7716613e
fantasylxh <772932587@qq.com>
2016-06-07 16:47:14 +0800

    修改图片域名

--- common/config/main.php
+++ common/config/main.php
@@ -20,6 +20,6 @@ return [
     ],
     'aliases' => [
         '@common/logic' => '@common/models/logic',
-        '@static' => 'http://admin.app.dev',
+        '@static' => 'http://admin.test.ohmate.com.cn',
     ],
 ];
