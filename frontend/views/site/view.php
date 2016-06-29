0d3702e309330387634a3f54abef7c9b4b4ba496
fantasylxh <772932587@qq.com>
2016-06-29 14:27:49 +0800

    git commit -a

--- frontend/views/site/view.php
+++ frontend/views/site/view.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no">
-    <title>文章详情</title>
+    <title><?php echo $data['title']?></title>
     <link href="http://vjs.zencdn.net/4.12/video-js.css" rel="stylesheet">
 </head>
 <style>
