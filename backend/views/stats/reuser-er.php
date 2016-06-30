0ae14b0fe7724a963ecc2ad04c348ee858a4b193
mime <zhaiyu_hb@hotmail.com>
2016-06-30 14:32:14 +0800

    资源统计

--- backend/views/stats/reuser-er.php
+++ backend/views/stats/reuser-er.php
@@ -81,35 +81,3 @@ backend\assets\AppAsset::register($this);
         </div>
     </div>
 </div>
-
-<?php
-$js = <<<JS
-$('#type-list-search').chosen({width: '240px'});
-var start = {
-    elem: '#startTime',
-    format: 'YYYY/MM/DD hh:mm:ss',
-    min: '2000-00-00 00:00:00', //设定最小日期为当前日期
-    max: '2099-06-16 23:59:59', //最大日期
-    istime: true,
-    istoday: false,
-    choose: function(datas){
-         end.min = datas; //开始日选好后，重置结束日的最小日期
-         end.start = datas //将结束日的初始值设定为开始日
-    }
-};
-var end = {
-    elem: '#endTime',
-    format: 'YYYY/MM/DD hh:mm:ss',
-    min: '2000-00-00 00:00:00',
-    max: '2099-06-16 23:59:59',
-    istime: true,
-    istoday: false,
-    choose: function(datas){
-        start.max = datas; //结束日选好后，重置开始日的最大日期
-    }
-};
-laydate(start);
-laydate(end);
-JS;
-$this->registerJs($js);
-?>
