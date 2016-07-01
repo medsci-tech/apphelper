8e82040bc376e143cc97b91f31738b76d616889c
mime <zhaiyu_hb@hotmail.com>
2016-07-01 11:47:16 +0800

    pdf2img

--- common/models/Upload.php
+++ common/models/Upload.php
@@ -30,9 +30,15 @@ class Upload extends Model
      */
     public function excel($uploadPath)
     {
-        $suffix = mb_substr($this->file->name, (mb_strripos($this->file->name, '.') + 1));
-        if(in_array($suffix,['xls','xlsx'])){
-            $fileName = $uploadPath .'/temp/'. date('YmdHis') . '.' . $suffix;
+        $name = date('YmdHis') . rand(1000,9999);
+        $suffix = mb_substr($this->file->name, mb_strripos($this->file->name, '.'));
+        if(in_array($suffix,['.xls','.xlsx'])){
+            $filePath = $uploadPath .'/temp/';
+            if(!file_exists($filePath)){
+                @mkdir($filePath);
+                @touch($filePath . 'index.html');
+            }
+            $fileName = $filePath . $name . $suffix;
             $res = $this->file->saveAs($fileName);
             if($res){
                 $return = ['code'=>200,'msg'=>'success','data'=>$fileName];
@@ -52,20 +58,47 @@ class Upload extends Model
      */
     public function image($uploadPath){
         $name = date('YmdHis') . rand(1000,9999);
-        $suffix = mb_substr($this->file->name, (mb_strripos($this->file->name, '.') + 1));
-        if(in_array($suffix,['png', 'jpg', 'gif'])){
-            $filePath = $uploadPath .'/examImg/';
+        $suffix = mb_substr($this->file->name, mb_strripos($this->file->name, '.'));
+        if(in_array($suffix,['.png', '.jpg', '.gif', '.bmp'])){
+            $filePath = $uploadPath .'/temp/';
             if(!file_exists($filePath)){
                 @mkdir($filePath);
                 @touch($filePath . 'index.html');
             }
-            $fileName = $filePath . $name . '.' . $suffix;
+            $fileName = $filePath . $name . $suffix;
             $res = $this->file->saveAs($fileName);
             if($res){
                 $return = ['code'=>200,'msg'=>'上传成功哦','data'=>[
                     'path' => $filePath,
                     'name' => $name . '.' . $suffix,
                 ]];
+            }else{
+                $return = ['code'=>701,'msg'=>'上传失败哦','data'=>''];
+            }
+        }else{
+            $return = ['code'=>703,'msg'=>'目前只支持png,jpg,gif格式','data'=>''];
+        }
+        return $return;
+    }
+
+    /**
+     * 图片上传
+     * @param $uploadPath
+     * @return array
+     */
+    public function pdf($uploadPath){
+        $name = date('YmdHis') . rand(1000,9999);
+        $suffix = mb_substr($this->file->name, mb_strripos($this->file->name, '.'));
+        if(in_array($suffix,['.pdf'])){
+            $filePath = $uploadPath .'/temp/';
+            if(!file_exists($filePath)){
+                @mkdir($filePath);
+                @touch($filePath . 'index.html');
+            }
+            $fileName = $filePath . $name . $suffix;
+            $res = $this->file->saveAs($fileName);
+            if($res){
+                $return = ['code'=>200,'msg'=>'上传成功哦','data' => $fileName];
             }else{
                 $return = ['code'=>701,'msg'=>'上传失败哦','data'=>''];
             }
