970a0853c939385bec385f6f4dd05a55279e96b2
zhaiyu <zhaiyu_hb@hotmail.com>
2017-01-10 16:44:08 +0800

    整理表结构

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -3,22 +3,22 @@ return [
     'components' => [
         //数据库存配置
         'db' => require(__DIR__ . '/db.php'),
-        'redis' => [
-            'class' => 'yii\redis\Connection',
-            'hostname' => '115.28.93.36',
-            'password' => 'md_root',
-            'port' => 6379,
-            'database' => 0,
-        ],
-        'cache' => [
-            'class' => 'yii\redis\Cache',
-        ],
+//        'redis' => [
+//            'class' => 'yii\redis\Connection',
+//            'hostname' => '115.28.93.36',
+//            'password' => 'md_root',
+//            'port' => 6379,
+//            'database' => 0,
+//        ],
+//        'cache' => [
+//            'class' => 'yii\redis\Cache',
+//        ],
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        'session' => [
-            'class' => 'yii\redis\Session',
-        ],
+//        'session' => [
+//            'class' => 'yii\redis\Session',
+//        ],
         'mail' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
